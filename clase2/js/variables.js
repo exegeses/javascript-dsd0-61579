00b4116cee977c557feaad911d267cc6cf7da8bd
Marcos <exegeses@gmail.com>
2022-08-16 20:41:47 -0300

    teoría de variables y contantes

---
+++ clase2/js/variables.js
@@ -0,0 +1,13 @@
+// para crear variables, usamos la palabra "let"
+let numero = 6;
+let frase = 'hola mundo';
+console.log( numero );
+console.log( frase );
+// modificamos el valor de las variables
+numero = 10;
+console.log( numero );
+
+//para crear contantes, usamos la palabra "const"
+const nombre = 'marcos';
+console.log( nombre );
+//nombre = 'ihty'; no puedo sobreescrir una constante
