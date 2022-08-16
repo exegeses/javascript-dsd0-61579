22712b6c06d77eafcabd0c8f5815a7427ce89dd0
Marcos <exegeses@gmail.com>
2022-08-16 20:00:27 -0300

    práctica innerText

---
+++ clase2/js/imprimir.js
@@ -0,0 +1,5 @@
+//escribir el final del documento
+document.write('esto va al final');
+
+//ubicamos elemento dento del DOM por su id
+document.getElementById('txt').innerText = 'esto es un texto desde javascript';
