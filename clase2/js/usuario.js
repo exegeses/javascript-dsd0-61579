168559d5804a5f1e5d165bc9548c61e93f219274
Marcos <exegeses@gmail.com>
2022-08-16 22:02:30 -0300

    práctica con constantes y variables

---
+++ clase2/js/usuario.js
@@ -0,0 +1,8 @@
+/*
+    pedimos al usuario que ingrese su nombre
+    almacenamos en una variable
+    luego lo imprimimos en el docuemnto
+*/
+let nombre = prompt('Ingrese su nombre');
+
+document.write( nombre );
